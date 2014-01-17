796668417e64d060e8f0f6797aee1d0056fa93f0
Peder Schacht <peteschacht@gmail.com>
2014-01-17 13:49:26 -0600

    jQuery SuperCalc 90% finished and jQuery Photoshop inital commit

--- 2014-01-16-jQuery.SuperCalc/public/js/main.js
+++ 2014-01-16-jQuery.SuperCalc/public/js/main.js
@@ -2,6 +2,46 @@ $(document).ready(initalize);
 
 function initalize(){
   $('.number').click(displayNumber);
+  $('#sign').click(changeSign);
+  $('#push').click(pushToQueue);
+  $('.operator').click(compute);
+  $('#clear').click(restart);
+}
+
+function compute(){
+  var operator = this.id;
+  var $lis = $('#queue li');
+  var numbers = parseTags($lis);
+  var result;
+  switch(operator){
+    case 'add':
+      result = numbers[0] + numbers[1];
+      break;
+    case 'sub':
+      result = numbers[1] - numbers[0];
+      break;
+    case 'mul':
+      result = numbers[0] * numbers[1];
+      break;
+    case 'div':
+      result = numbers[1] /  numbers[0];
+      break;
+    case 'sum':
+      result = numbers[0] +  numbers[1];
+      break;
+    case 'pow':
+      result = Math.pow(numbers[1], numbers[0]);
+  }
+  $('#answer').text(result);
+  $('#queue').empty();
+}
+
+function pushToQueue(){
+  var display = $('#answer').text();
+  $('#answer').text('0');
+  var $li = $('<li>');
+  $li.text(display);
+  $('#queue').prepend($li);
 }
 
 function displayNumber(){
@@ -9,7 +49,7 @@ function displayNumber(){
   var current = this.textContent;
   var output;
 
-  if(current === '.' && display.indexOf('.') !== -1)return;
+  if(current === '.' && containsChar(display, '.'))return;
 
   if(display === '0' && current !== '.')
     output = current;
@@ -17,5 +57,16 @@ function displayNumber(){
     output = display + current;
   
  $('#answer').text(output);
+}
+
+function changeSign(){
+  var number = $('#answer').text();
+  number *= -1;
+  $('#answer').text(number);
+}
+
+function restart(){
+  $('#answer').text('0');
+  $('#queue').empty();
 }
 
