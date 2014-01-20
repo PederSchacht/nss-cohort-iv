fd4d3be6a3b1b6f982b5b6cd0930aeb063d04aa0
Peder Schacht <peteschacht@gmail.com>
2014-01-20 01:31:21 -0600

    Photoshop project functional and visualy styled, ready for class viewing

--- 2014-01-17-jQuery.Photoshop/public/js/main.js
+++ 2014-01-17-jQuery.Photoshop/public/js/main.js
@@ -1,47 +1,46 @@
 $(document).ready(initialize);
 
 function initialize(){
-  $('#add-color').click(clickAddColor);
-  $('#add-pixels').click(clickAddPixels);
-  $('#colors').on('click', '.color', clickSelectColor);
-  $('#pixels').on('mouseover', '.pixel', hoverColorPixel);
+    $('#add-color').click(clickAddColor);
+    $('#add-pixels').click(clickAddPixels);
+    $('#colors').on('click', '.color', clickSelectColor);
+    $('#pixels').on('mouseover', '.pixel', hoverColorPixel);
 }
 
 function hoverColorPixel(){
-  var color = $('.seleced').css('background-color');
-  $(this).css('background-color',color);
+    var color = $('.selected').css('background-color');
+    $(this).css('background-color', color);
 }
 
 function clickAddPixels(){
-    var num = $('#number-text).val();
-    num = parseInt(num);
+  var num = $('#number-text').val();
+  num = parseInt(num);
 
-    for(var i = 0, i < num; i++){
-      var $pixel = $('<div>');
-      $pixel.addClass('pixel');
-      $('#pixels').prepend($pixel);
-    }
+  for(var i = 0; i < num; i++){
+    var $pixel = $('<div>');
+    $pixel.addClass('pixel');
+    $('#pixels').prepend($pixel);
+  }
+}
+
+function clickSelectColor(){
+  if($(this).hasClass('selected')){
+    $(this).removeClass('selected');
+  } else {
+    $('.color').removeClass('selected');
+    $(this).addClass('selected');
+  }
 }
 
 function clickAddColor(){
   var color = $('#color-text').val();
   $('#color-text').val('');
   $('#color-text').focus();
-  
+
   var $box = $('<div>');
   $box.addClass('color');
-  $box.css('background-color', color)
+  $box.css('background-color', color);
 
   $('#colors').prepend($box);
-}
-
-
-function clickSelectColor(){
- if($(this).hasClass('selected')){
-   $(this).removeClass('selected');
- } else {
-     $('.color').removeClass('selected');
-     $(this).addClass('selected');
-   }
 }
 
